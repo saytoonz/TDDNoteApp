63916a1dea8fcbb76e70600e9379283e23fc17c8
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-03-21 15:21:43 +0000

    Notes screen

---
+++ app/src/main/java/com/saytoonz/tddnoteapp/feature_note/presentation/notes/NoteScreen.kt
@@ -0,0 +1,124 @@
+package com.saytoonz.tddnoteapp.feature_note.presentation.notes
+
+import androidx.compose.animation.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Sort
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.saytoonz.tddnoteapp.feature_note.presentation.notes.components.NoteItem
+import com.saytoonz.tddnoteapp.feature_note.presentation.notes.components.OrderSection
+import kotlinx.coroutines.launch
+
+@Composable
+fun NoteScreen(
+    navController: NavController,
+    viewModel: NotesViewModel = hiltViewModel()
+) {
+    val state = viewModel.state.value
+    val scaffoldState = rememberScaffoldState() // to show snack bars
+    val coroutineScope = rememberCoroutineScope() //actually needed to show snack bars
+
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                backgroundColor = MaterialTheme.colors.primary
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add new note")
+            }
+        }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(contentPadding)
+                .padding(16.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Your note",
+                    style = MaterialTheme.typography.h4
+                )
+                
+                IconButton(
+                    onClick = {
+                        viewModel.onEvent(NotesEvent.ToggleOrderSection)
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Sort,
+                        contentDescription = "Sort"
+                    )
+                }
+            }
+            
+            AnimatedVisibility(
+                visible = state.isOrderSectionVisible,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically()
+            ) {
+                OrderSection(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 16.dp),
+                    noteOrder = state.noteOrder,
+                    onOrderChange = { noteOrder ->
+                        viewModel.onEvent(NotesEvent.Order(noteOrder))
+                    }
+                )
+            }
+            
+            
+            Spacer(modifier = Modifier.height(16.dp))
+            
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ){
+                items(state.notes){note ->
+                    NoteItem(
+                        note = note,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+
+                            },
+                        onDeleteClick = {
+                            viewModel.onEvent(NotesEvent.DeleteNote(note))
+                            coroutineScope.launch {
+                               val result = scaffoldState.snackbarHostState.showSnackbar(
+                                    message = "Note Deleted",
+                                    actionLabel = "Undo"
+                                )
+                                if (result == SnackbarResult.ActionPerformed){
+                                    viewModel.onEvent(NotesEvent.RestoreNote)
+                                }
+                            }
+                        },
+
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                }
+            }
+            
+        }
+    }
+
+}
